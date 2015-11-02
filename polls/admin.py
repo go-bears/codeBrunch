d680d650f20e087591674f6874c837a938f8ede2
go-bears <melissa.fabros@gmail.com>
2015-11-02 03:27:38 +0000

    add polls views and new html files

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Question, Choice
+
+
 
 # Register your models here.
+
+#Create class for writing questions choices from admin page
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+#registers question mode from models.py as an object for the admin file
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+
+#make question editable from admin page
+admin.site.register(Question, QuestionAdmin)
+ 
+#makes question choices editable from admin page
+admin.site.register(Choice)
+
+
